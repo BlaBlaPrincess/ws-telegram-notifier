0ad3d738dff1bb46922d78741e595b376c90ad71
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-17 22:07:27 +1000

    Initial commit

---
+++ src/test/java/com/github/blablaprincess/telegramnotifier/ApplicationSpringIT.java
@@ -0,0 +1,13 @@
+package com.github.blablaprincess.telegramnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSpringIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
